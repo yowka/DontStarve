3d9e71d5dda33b47da802f81317d4020f49b380b
yowka <stasskirnevskiy@gmail.com>
2024-05-28 17:58:13 +0700

    git commit --amend

--- src/components/component/theHeader.vue
+++ src/components/component/theHeader.vue
@@ -4,15 +4,15 @@
 
 <template>
   <div class="general">
-      <div class="header">
+      <div class="head">
         <div class="general_logo">
-        <router-link class="link" to="/"><img class="logo" alt="#" src="/src/assets/image/logotype.png"></router-link>
+        <router-link class="link" to="/"><div class="logo"></div></router-link>
         </div>
         <div>
           <router-link class="link" to="/about">Подробнее</router-link>
         </div>
         <div>
-          <router-link class="link" to="/register"><button>Войти</button></router-link>
+          <router-link class="link3" to="/register"><button>Войти</button></router-link>
           <router-link class="link" to="/register"><button>Регистрация</button></router-link>
         </div>
       </div>
@@ -20,27 +20,36 @@
 </template>
 
 <style scoped>
-.header{
+.head{
   margin: 0 auto;
-  width: 90%;
-  background: darkgray;
-  border-radius: 10px;
-  height: 100px;
-  opacity: 80%;
+  background: #7d5e5e;
+  height: 200px;
   display: flex;
   align-items: center;
   justify-content: space-around;
+
 }
-.logo{
-  height: 100px;
+.general::before{
+  background:url("/src/assets/image/logotype.png") no-repeat ;
+  height:18px;
 }
-.link{
-  color: darkslategrey;
+.link,.link3{
+  color: #3C0D2A;
   font-size: 40px;
   text-decoration: none;
 }
-
-.link button{
+.link3{
+  margin-right: 30px;
+}
+.logo{
+  width: 240px;
+  height: 200px;
+  background: url("/src/assets/image/logotype.png") no-repeat center center;
+  background-size: cover;
+}
+.link button,.link3 button{
   font-size: 30px;
+  background: rgb(256,256,256,0.4);
+  border-radius: 5px;
 }
 </style>
--- src/components/pages/State.vue
+++ src/components/pages/State.vue
@@ -1,10 +1,50 @@
 <script setup>
 
 </script>
-
 <template>
+  <div class="general">
+    <div class="container_block">
+      <div class="main_block">
+        fdghdfh
+      </div>
+      <aside class="aside_block">
+        <img alt="#" src="/src/assets/image/trailer.jpg"/>
+      </aside>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+.general{
+  padding: 60px;
+}
+.container_block {
+  border-radius: 20px;
+  margin: 0 auto;
+  width: 88%;
+  height: 1920px;
+  background: rgb(234, 222, 200 ,0.9);
+  grid-template-columns: 3fr 1fr;
+  display: grid;
+  justify-items: center;
+  align-items: center;
+}
+.main_block,.aside_block{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+  height: 98%;
+  border-radius: 20px;
+  background: rgba(44, 44, 44, 0.84);
+}
+.main_block{
+  width: 96%;
+}
+.aside_block{
+  width: 90%;
+}
+.container_block img{
 
+}
 </style>
