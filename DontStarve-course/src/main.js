8c4f42e2c5c363eef9251bd373f2b498760411c1
yowka <stasskirnevskiy@gmail.com>
2024-05-30 21:54:48 +0700

    git commit --amend

--- DontStarve-course/src/main.js
+++
@@ -1,6 +0,0 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from '../src/components/router/routing.js'
-createApp(App).use(router).mount('#app')
